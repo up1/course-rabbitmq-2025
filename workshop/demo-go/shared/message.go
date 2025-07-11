b91730ac9d879a8fb0b734781ad38143c8010e57
up1 <somkiat.p@gmail.com>
2025-07-11 11:29:38 +0700

    Update structure of message

--- workshop/demo-go/shared/message.go
+++ workshop/demo-go/shared/message.go
@@ -3,8 +3,9 @@ package shared
 type Message struct {
 	ID      string `json:"id"`
 	Content string `json:"content"`
+	Catalog string `json:"catalog,omitempty"` // Optional routing key
 }
 
 func (m *Message) String() string {
-	return "ID: " + m.ID + ", Content: " + m.Content
+	return "ID: " + m.ID + ", Content: " + m.Content + ", Catalog: " + m.Catalog
 }
